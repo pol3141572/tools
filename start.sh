7f10815b731ba12c0b93af23428f92647e0a0f1c
pol3141572 <x9vzpbkp2p@privaterelay.appleid.com>
2026-05-19 13:14:10 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "intialising tools"
+source aliassen.sh
+source functies.sh
+source .myzshrc
+echo "initialised tools"
+echo "welkom Pol"
+#end of file
+#produces by:
+# ██████╗  ██████╗ ██╗
+# ██╔══██╗██╔═══██╗██║
+# ██████╔╝██║   ██║██║
+# ██╔═══╝ ██║   ██║██║
+# ██║     ╚██████╔╝███████╗
+# ╚═╝      ╚═════╝ ╚══════╝
